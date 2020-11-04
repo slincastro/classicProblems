cdf8d5bf630f3636ce7c60b9bfb18a3a01211860
Slin Castro <scastro@thoughtworks.com>
2020-11-03 22:04:11 -0500

    Include fibonacci genration

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,11 @@
 from typing import Dict
+from typing import Generator
+
 memo: Dict[int, int] = {0: 0, 1: 1}
 
-def fib_in_memmory (n: int) -> int:
+def fib_in_memory (n: int) -> int:
     if n not in memo:
-        memo[n] = fib_in_memmory(n - 1) + fib_in_memmory(n - 2)
+        memo[n] = fib_in_memory(n - 1) + fib_in_memory(n - 2)
     return memo[n]
 
 
@@ -13,10 +15,22 @@ def fib_recursive(n: int) -> int:
     next: int = 1
 
     for _ in range(1, n):
-        last, next = next, last + next
+        last, next = next, last + next #review the tuple unpacking here
 
     return next
 
+def fib_generation(n: int) -> Generator[int, None, None] :
+    yield 0
+    if n > 0 : yield 1
+    last: int = 0
+    next: int = 1
+    for _ in range(1, n):
+        last, next = next, last + next
+        yield next
 
-print(fib_in_memmory(10))
+
+print(fib_in_memory(10))
 print(fib_recursive(10))
+
+for i in fib_generation(10):
+    print(i)
